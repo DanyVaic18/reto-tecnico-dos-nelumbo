a6529213b40c1576371f0fa4a331a1ca5f729a9b
DanyVaic18 <loljose1090@gmail.com>
2024-10-07 13:04:25 -0500

    feat: test taildwind css in proyect

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Pagina principal</h1>
+      <h1 className="text-red-600">Pagina principal</h1>
     </div>
   );
 };
