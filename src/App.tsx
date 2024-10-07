ba83f674abfe91266a8ca0d501e662f9f5dffcf1
DanyVaic18 <loljose1090@gmail.com>
2024-10-07 12:42:28 -0500

    feat: create components init proyect NotFound and Home

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./providers/theme";
 import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
 
 function App() {
   return (
@@ -11,24 +13,8 @@ function App() {
         <HashRouter //basename="/reto-tecnico-dos-nelumbo"
         >
           <Routes>
-            <Route
-              index
-              path="/"
-              element={
-                <>
-                  <h1>Pagina principal</h1>
-                </>
-              }
-            />
-            <Route
-              path="/dos"
-              element={
-                <>
-                  <h2>HOLA MUNDO</h2>
-                </>
-              }
-            />
-            <Route path="/*" element={<h2>Pagina no encontrada</h2>} />
+            <Route index path="/" element={<Home />} />
+            <Route path="/*" element={<NotFound />} />
           </Routes>
         </HashRouter>
       </ThemeProvider>
