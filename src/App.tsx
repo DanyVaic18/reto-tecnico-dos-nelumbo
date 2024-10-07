89c99b6832587c0ed62781a9f8d10c8bd5f8ec9a
DanyVaic18 <loljose1090@gmail.com>
2024-10-07 12:30:20 -0500

    feat: Route configuration added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./providers/theme";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <HashRouter //basename="/reto-tecnico-dos-nelumbo"
+        >
+          <Routes>
+            <Route
+              index
+              path="/"
+              element={
+                <>
+                  <h1>Pagina principal</h1>
+                </>
+              }
+            />
+            <Route
+              path="/dos"
+              element={
+                <>
+                  <h2>HOLA MUNDO</h2>
+                </>
+              }
+            />
+            <Route path="/*" element={<h2>Pagina no encontrada</h2>} />
+          </Routes>
+        </HashRouter>
       </ThemeProvider>
     </>
   );
